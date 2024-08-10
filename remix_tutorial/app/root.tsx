d2146abd75acdd649e37411b031aa8ad2bf6c7f6
DPontaro <n.douzono@gmail.com>
2024-08-10 23:53:25 +0900

    editできるようになるまで

--- remix_tutorial/app/root.tsx
+++ remix_tutorial/app/root.tsx
@@ -1,19 +1,39 @@
 import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import appStylesHref from "./app.css?url";
+
+import { createEmptyContact, getContacts } from "./data";
+
+export const action = async () => {
+  console.log('call action');
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref }
 ];
 
+export const loader = async () => {
+  const contacts = await getContacts();
+  console.log('call loader');
+  return json({ contacts });
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+  console.log('App');
+
   return (
     <html lang="en">
       <head>
@@ -41,15 +61,31 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+            </nav>
         </div>
         <div id="detail">
           <Outlet />
